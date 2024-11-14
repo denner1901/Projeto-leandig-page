973e36043593ca505c38721a46b5d9813583380d
dennerson higor sousa vieira <dennerson1234@live.com>
2024-11-14 20:28:29 -0300

    versão inicial

---
+++ index.js
@@ -0,0 +1,24 @@
+function RolarParaDireita(){  
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var bruna = window.document.getElementById("bruna")
+var setadireita = window.document.getElementById("seta-direita")
+var setaesquerda = window.document.getElementById("seta-esquerda")
+
+    leonardo.style = "display:none"
+    bruna.style = "display:flex"
+    setadireita.style = "display:none; margin-top:55px"
+    setaesquerda.style = "display:flex; margin-top:55px"
+}
+function RolarParaEsquerda(){
+    var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var bruna = window.document.getElementById("bruna")
+var setadireita = window.document.getElementById("seta-direita")
+var setaesquerda = window.document.getElementById("seta-esquerda")
+
+    leonardo.style = "display:flex"
+    bruna.style = "display:none"
+    setadireita.style = "display:flex; margin-top:55px"
+    setaesquerda.style = "display:none; margin-top:55px"
+}
